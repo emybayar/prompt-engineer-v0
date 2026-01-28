fd3b44dbe8c41e5425da598d5b87830b27d1c9b1
emybayar <es5657@nyu.edu>
2026-01-28 16:44:35 +0000

    vite path

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  base: process.env.VITE_BASE_PATH || "/prompt-engineer-v0/tree/main",
 });
 
 // module.exports = {
