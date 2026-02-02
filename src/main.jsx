6354faf26a5a5306d3e81a0d2601af5e5c5f80ec
emybayar <es5657@nyu.edu>
2026-02-02 14:48:44 +0000

    updated analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import Engineer from "./Engineer.jsx";
-import { Analytics } from "@vercel/analytics/next";
-import App from "./App.jsx";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
     <Engineer />
+    <Analytics />
   </StrictMode>,
 );
